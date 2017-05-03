66f2ba587ffd5d4f8c784a5cb80093423b611ac3
Aurélien HUGELE <altimac@carrafix.com>
2017-05-03 14:16:58 +0200

    - Initial project setup

---
+++ GroundControlAdmin/GroundControlAdmin/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GroundControlAdmin
+//
+//  Created by Aurélien Hugelé on 03/05/2017.
+//  Copyright © 2017 GroundControl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GroundControlAdmin/GroundControlAdmin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GroundControlAdmin
+//
+//  Created by Aurélien Hugelé on 03/05/2017.
+//  Copyright © 2017 GroundControl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

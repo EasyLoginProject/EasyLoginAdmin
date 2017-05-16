a4a631201f07160b3302c5ba998d4fcada14dc4d
Aurélien HUGELE <altimac@carrafix.com>
2017-05-16 18:02:28 +0200

    - Quick'n dirty embedded test

--- EasyLoginAdmin/EasyLoginAdmin/UsersViewController.swift
+++ EasyLoginAdmin/EasyLoginAdmin/UsersViewController.swift
@@ -14,8 +14,24 @@ class UsersViewController: NSViewController {
     
     var webServiceConnector: ELWebServiceConnector?
     
+    
+    required init?(coder: NSCoder) {
+        self.webServiceConnector = ELWebServiceConnector(baseURL:URL(string:"http://easylogin.imoof.com:9789/whatever/v1/")!, headers: nil)
+        
+        super.init(coder:coder)
+    }
+    
     override func viewDidLoad() {
+        
+        if let op = webServiceConnector?.getUserPropertiesOperation(forUserUniqueId: "2e88e5f2d5c748bbb839124f5ab3da24", completionBlock: { (userProperties: [String : Any]?, operation: CFXNetworkOperation) in
+            if let userProperties = userProperties {
+                print(userProperties);
+            }
+        }) {
+            webServiceConnector?.enqueue(op)
+        }
+        
         super.viewDidLoad()
-        // Do view setup here.
+        
     }
 }

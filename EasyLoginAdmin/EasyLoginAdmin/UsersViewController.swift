671b988c1711f3d3c2d7553eb8409335209903ee
Aurélien HUGELE <altimac@carrafix.com>
2017-06-09 16:34:13 +0200

    - tested bindings approach, but keeping the ELRecordProperties option

--- EasyLoginAdmin/EasyLoginAdmin/UsersViewController.swift
+++ EasyLoginAdmin/EasyLoginAdmin/UsersViewController.swift
@@ -32,17 +32,39 @@ class UsersViewController: NSViewController {
     
     override func viewDidLoad() {
         
-        if let op = webServiceConnector?.getUserPropertiesOperation(forUserUniqueId: "5d30d66df8414c2daf04ba70a654edc6", completionBlock: { (userProperties: [String : Any]?, operation: CFXNetworkOperation) in
-            if let userProperties = userProperties {
-                print(userProperties);
-            }
-        }) {
-            webServiceConnector?.enqueue(op)
-        }
+//        if let op = webServiceConnector?.getUserPropertiesOperation(forUserIdentifier: "5d30d66df8414c2daf04ba70a654edc6", completionBlock: { (userProperties: [String : Any]?, operation: CFXNetworkOperation) in
+//            if let userProperties = userProperties {
+//                print(userProperties);
+//            }
+//        }) {
+//            webServiceConnector?.enqueue(op)
+//        }
         
         if let op = webServiceConnector?.getAllUsersOperation(completionBlock: { (users, op) in
             if let users = users {
                 self.usersArrayController.content = users;
+                
+                
+                // NOTE: we should block user edits until all user properties are cached
+                let parallelWSC = self.webServiceConnector?.copy() as! ELWebServiceConnector
+                parallelWSC.maxConcurrentOperationCount = 10;
+                
+                for user in users {
+                    let op = parallelWSC.getUserPropertiesOperation(forUserIdentifier: user.identifier(), completionBlock: { (userProperties, op) in
+                        if let userProperties = userProperties {
+                            print(userProperties);
+                            if let recordProperties = ELRecordProperties(dictionary: userProperties, mapping: nil) {
+                                #if USE_OBJC_PROPERTIES
+                                user.__transfer(from: recordProperties)
+                                #else
+                                user.update(with: recordProperties, deletes: true)
+                                #endif
+                            }
+                        }
+                    })
+                    
+                        parallelWSC.enqueue(op)
+                }
             }
         }) {
             webServiceConnector?.enqueue(op)
@@ -50,5 +72,12 @@ class UsersViewController: NSViewController {
         
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func addUserButtonActivated(_ sender: Any) {
+        //let newUser = ELUser(properties: ELRecordProperties(dictionary: <#T##[AnyHashable : Any]#>, mapping: nil)
+    }
+    
+    @IBAction func deleteUserButtonActivated(_ sender: Any) {
     }
 }
